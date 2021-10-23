351f77a2df8372cb74df1e556017a77ab23e4f1f
Esteban Frangiosa <87097754+F3N1X1310@users.noreply.github.com>
2021-10-23 13:12:40 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Obtener los pasos (steps)
+const steps = document.querySelectorAll(".step");
+
+// Obtener la barra de progreso
+const bar = document.getElementById("bar");
+
+// Obtener la referencia a los botones
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+// Guardar el paso en el que estamos
+let currentStep = 1;
+
+// Escuchar el botón siguiente
+nextButton.addEventListener("click", () => {
+  if (currentStep < steps.length) {
+    // Sumar 1 a los pasos
+    currentStep++;
+    updateProgress();
+  }
+});
+
+// Escuchar el boton anterior
+prevButton.addEventListener("click", () => {
+  if (currentStep > 1) {
+    // Restar 1 a los pasos
+    currentStep--;
+    updateProgress();
+  }
+});
+
+// Función para actualizar el progreso
+function updateProgress() {
+  // Actualizar pasos
+  steps.forEach((step, index) => {
+    if (index < currentStep) {
+      step.classList.add("active");
+    } else {
+      step.classList.remove("active");
+    }
+  });
+
+  // Actualizar barra
+  const progress = (currentStep - 1) / (steps.length - 1) * 100
+  bar.style.width = progress + "%"
+
+  // Control de los botones
+  if (currentStep === steps.length) {
+    nextButton.disabled = true
+  } else if (currentStep === 1) {
+    prevButton.disabled = true;
+  } else {
+    prevButton.disabled = nextButton.disabled = false;
+  }
+}
